2613f002e176250366905969bd11b78da5413dd4
Jerry <calljerryli@outlook.com>
2021-12-04 10:53:11 +0800

    抽离 mockData.js

---
+++ src/mock/mockData.js
@@ -0,0 +1,23 @@
+const memoList = [
+    {
+        id: '3',
+        content: '2016年，WhatsApp 的用户超过10亿，但是只有50个工程师。每个小团队由1到3名工程师组成，拥有很大的自主权。\n-- https://www.quastor.org/p/how-whatsapp-scaled-to-1-billion',
+        createdAt: '2021-12-01 14:29:24',
+    },
+    {
+        id: '2',
+        content: '一个可运行的复杂系统，总是从一个简单系统演变而来的。似乎可以因此推断：从头开始设计一个复杂系统，永远不会奏效，必须从一个简单系统开始设计。\n-- https://www.ivanmontilla.com/blog/galls-law-and-how-i-ignored-it',
+        createdAt: '2021-10-25 17:51:25',
+    },
+    {
+        id: '1',
+        content: '切勿交浅言深',
+        createdAt: '2021-10-06 17:49:11',
+    },
+]
+
+const mockData = {
+    memoList,
+}
+
+export default mockData
