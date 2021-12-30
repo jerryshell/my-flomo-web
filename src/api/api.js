9a4c3a54928f216f9e83cde69d6d123d2272690a
Jerry <calljerryli@outlook.com>
2021-12-30 18:35:15 +0800

    axios 请求拦截器

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
     baseURL: "http://localhost:8080/",
 });
 
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.token = token;
+    }
+    return config;
+});
+
 export default api
