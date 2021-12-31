070dc4c0b4806a0deb4660592bf4e780479b77f1
Jerry <calljerryli@outlook.com>
2021-12-31 18:26:33 +0800

    处理 response 401

--- src/api/api.js
+++ src/api/api.js
@@ -4,12 +4,22 @@ const api = axios.create({
     baseURL: "http://localhost:8080/",
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use(async (config) => {
     const token = localStorage.getItem('token');
     if (token) {
         config.headers.token = token;
     }
     return config;
+});
+
+api.interceptors.response.use(async (response) => {
+    const token = localStorage.getItem('token');
+    if (response.data.code === 401 && token) {
+        localStorage.removeItem('token');
+        window.location.href = '/';
+        return Promise.reject(response);
+    }
+    return response;
 });
 
 export default api
