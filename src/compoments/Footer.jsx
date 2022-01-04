292476c457adcaa7629ad129949eee98d5dce63b
Jerry <calljerryli@outlook.com>
2022-01-04 15:09:35 +0800

    更新 Footer.jsx

--- src/compoments/Footer.jsx
+++ src/compoments/Footer.jsx
@@ -1,6 +1,10 @@
 const Footer = () => {
     return (
-        <p>Created by <a href="https://github.com/jerryshell" target='_blank'>Jerry</a></p>
+        <p>Created by
+            <a href="https://github.com/jerryshell" target='_blank'> @jerryshell </a>
+            &
+            <a href="https://github.com/zcyc" target='_blank'> @zcyc </a>
+        </p>
     )
 }
 
