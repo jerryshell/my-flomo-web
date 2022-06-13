7e66eed594fdf19cc0f9372c67fcffa5e5dcb18c
Jerry <calljerryli@outlook.com>
2022-06-13 14:25:48 +0800

    .env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: 'http://localhost:8060',
-    baseURL: 'https://my-flomo-api.d8s.fun',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 api.interceptors.request.use(async (config) => {
