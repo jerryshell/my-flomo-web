b4b82d2183f84ae41085b117c4b42f7ace297aa2
Charles <8595764@qq.com>
2022-10-20 22:08:13 +0800

    feat: show random api

--- src/compoments/PluginToken.tsx
+++ src/compoments/PluginToken.tsx
@@ -1,47 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import pluginApi from '../api/pluginApi'
-import api from '../api/api'
+import pluginApi from "../api/pluginApi";
+import api from "../api/api";
 
 const PluginToken = () => {
-    const [pluginToken, setPluginToken] = useState('')
-    const [errorMessage, setErrorMessage] = useState('')
+  const [pluginToken, setPluginToken] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const fetchPluginToken = () => {
-        pluginApi.getToken()
-            .then(response => {
-                const success = response.data.success
-                if (success) {
-                    setPluginToken(response.data.data)
-                } else {
-                    setPluginToken('')
-                    setErrorMessage(response.data.message)
-                }
-            })
-    }
+  const fetchPluginToken = () => {
+    pluginApi.getToken().then((response) => {
+      const success = response.data.success;
+      if (success) {
+        setPluginToken(response.data.data);
+      } else {
+        setPluginToken("");
+        setErrorMessage(response.data.message);
+      }
+    });
+  };
 
-    useEffect(() => {
-        fetchPluginToken()
-    }, [])
+  useEffect(() => {
+    fetchPluginToken();
+  }, []);
 
-    const handleGeneratePluginTokenBtnClick = () => {
-        pluginApi.createToken()
-            .then(response => {
-                setPluginToken(response.data.data)
-            })
-    }
+  const handleGeneratePluginTokenBtnClick = () => {
+    pluginApi.createToken().then((response) => {
+      setPluginToken(response.data.data);
+    });
+  };
 
-    return (
-        <details>
-            <summary>插件令牌</summary>
-            {
-                pluginToken
-                    ? <p>{ api.defaults.baseURL }/plugin/createMemo/{ pluginToken }</p>
-                    : <p>{ errorMessage }</p>
-            }
-            <button onClick={ handleGeneratePluginTokenBtnClick }>重新生成</button>
-        </details>
-    )
-}
+  return (
+    <details>
+      <summary>插件接口</summary>
+      {pluginToken ? (
+        <div>
+          <p>
+            新增记录: {api.defaults.baseURL}/plugin/createMemo/{pluginToken}
+          </p>
+          <p>
+            随机获取: {api.defaults.baseURL}/plugin/randomMemo/{pluginToken}
+          </p>
+        </div>
+      ) : (
+        <p>{errorMessage}</p>
+      )}
+      <button onClick={handleGeneratePluginTokenBtnClick}>重新生成</button>
+    </details>
+  );
+};
 
-export default PluginToken
+export default PluginToken;
