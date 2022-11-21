2da270cccedf0d543554db34d4891376bb0b3b20
jerry <calljerryli@outlook.com>
2022-11-21 18:26:04 +0800

    Add ServerStatusCheck component

--- src/compoments/Header.tsx
+++ src/compoments/Header.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import logo from '../favicon.svg'
+import ServerStatusCheck from "./ServerStatusCheck";
 
 const Header = () => {
   return (
-    <h1>
-      <img
-        src={logo}
-        alt="logo"
-      />
-      <span> </span>
-      <span>My Flomo</span>
-    </h1>
+    <>
+      <h1>
+        <img
+          src={logo}
+          alt="logo"
+        />
+        <span> </span>
+        <span>My Flomo</span>
+      </h1>
+      <ServerStatusCheck/>
+    </>
   )
 }
 
---
+++ src/compoments/ServerStatusCheck.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import healthApi from "../api/healthApi";
+
+const ServerStatusCheck = () => {
+  const [serverStatus, setServerStatus] = useState('检查中')
+
+  useEffect(() => {
+    healthApi.health().then(response => {
+      console.log('health response', response)
+      const success = response.data.success
+      if (success) {
+        setServerStatus('在线')
+      } else {
+        setServerStatus('离线')
+      }
+    }).catch(e => {
+      console.error('health error', e)
+      setServerStatus('离线')
+    })
+  }, [])
+
+  return (
+    <>
+      <p>服务器状态：{serverStatus}</p>
+    </>
+  )
+}
+
+export default ServerStatusCheck
